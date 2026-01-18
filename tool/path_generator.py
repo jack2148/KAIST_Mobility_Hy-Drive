b8643b1df2ae5dcdfa124e4c4baa98156881d32b
chan <yeachan4842@gmail.com>
2026-01-18 22:27:22 +0900

    merge

--- tool/path_generator.py
+++ tool/path_generator.py
@@ -57,27 +57,9 @@ def generate_path_from_nodes(
 
 # ===== Example Usage =====
 if __name__ == "__main__":
-<<<<<<< HEAD
     nodes = [21, 51, 46, 40, 63, 34, 27, 28, 31, 1, 3, 6, 7, 10, 9,56, 59, 18, 21] # <-- modify path if needed
     waypoint_dir = "waypoint"  
     output_csv = "p1_1_path.csv"
 
-=======
-    # nodes = [18, 21, 51, 46, 40, 63, 34, 27, 31, 1, 3, 7, 9, 56, 59, 18] # CAV1 p1
-    # nodes = [15, 60, 52, 24, 27, 30, 1, 3, 6, 44, 63, 39, 49, 55, 12, 15] # CAV2 p1
-    nodes = [28,31,1] 
-    # nodes = [18, 21, 51, 46, 41, 43, 9, 56, 59, 18] # CAV1 p3
-    # nodes = [15, 60, 52, 24, 37, 38, 49, 55, 12, 15] # CAV2 p3
-    # nodes = [58, 19, 22, 25, 36, 38, 48, 58] # CAV3
-    # nodes = [11, 16, 61, 47, 41, 42, 8, 11] #CAV4
-    # nodes = [38, 41, 62, 38] # rotation_outside
-    # nodes = [46, 40, 43] # CAV1 inside
-    #nodes = [37, 39, 49] # CAV2 inside
-    # nodes = [36, 39, 48] # CAV3 inside
-    # nodes = [47, 40, 42] # CAV4 inside
-    # nodes = [39, 40, 63, 39] # roataion_inside
-    waypoint_dir = "waypoint"  
-    output_csv = "JSp2merge.csv"
->>>>>>> 0551b0759ad15a776d5f287027acaa35c16623a8
     generate_path_from_nodes(nodes, waypoint_dir, output_csv)
 
